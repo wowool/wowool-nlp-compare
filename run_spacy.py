596e64a925639bd4fccbd3efbccd8aa5df9685ca
phforest-wow <philippe@wowool.com>
2025-01-07 15:29:17 +0100

    added -i

--- run_spacy.py
+++ run_spacy.py
@@ -8,7 +8,8 @@ def parse_arguments():
     Parses the command line arguments.
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--file", help="input file", nargs="+", required=True)
+    parser.add_argument("-f", "--file", help="input file", nargs="*", required=False)
+    parser.add_argument("-i", "--input", help="input text")
     parser.add_argument("-m", "--model", help="input model")
 
     args = parser.parse_args()
@@ -17,16 +18,23 @@ def parse_arguments():
 
 if __name__ == "__main__":
     kwargs = dict(parse_arguments()._get_kwargs())
-
     engine = spacy.load(kwargs["model"])
-    files = kwargs.pop("file")
-    for filename in files:
-        fn = Path(filename)
-        if fn.exists():
-            text = fn.read_text()
-            doc = engine(text)
-            print("Entities:\n==========")
-            for ent in doc.ents:
-                print(f"{ent.text} - {ent.label_}")
-        else:
-            print(f"File not found: {fn}")
+    if "input" in kwargs:
+        text = kwargs.pop("input")
+        doc = engine(text)
+        print("Entities:\n==========")
+        for ent in doc.ents:
+            print(f"{ent.text} - {ent.label_}")
+    else:
+        files = kwargs.pop("file")
+
+        for filename in files:
+            fn = Path(filename)
+            if fn.exists():
+                text = fn.read_text()
+                doc = engine(text)
+                print("Entities:\n==========")
+                for ent in doc.ents:
+                    print(f"{ent.text} - {ent.label_}")
+            else:
+                print(f"File not found: {fn}")
